747d51c46521c8ffa15470c7db9cf6e847114275
nakatani-s <nakatani-s@roboken.iit.tsukuba.ac.jp>
2020-10-15 15:48:29 +0900

    version_1.2.5

--- MCMPC/mcmpc_src/MCMPC_Controller.cu
+++ MCMPC/mcmpc_src/MCMPC_Controller.cu
@@ -45,7 +45,8 @@ __global__ void MCMPC_GPU(float *h_state, SpecGPU gpu_info, curandState *devSt, 
     general_copy(Dev_State, h_state, DIM_X);
     for(int i = 0; i < NUM_OF_HORIZON; i++){
         for(int k = 0; k < DIM_U; k++){
-            Input_here[k] = generate_u1(id, devSt, InpSeq[k].u[i], st_dev[k]);
+            U_dev[k][i] = InpSeq[k].u[i];
+            Input_here[k] = generate_u1(id, devSt, U_dev[k][i], st_dev[k]);
             Input_here[k] = input_saturation(Input_here[k], input_constraint, k);
             U_dev[k][i] = Input_here[k]; //入力を生成する関数はここ（同じファイル）に記述しないと機能しない
         }
@@ -78,7 +79,7 @@ __global__ void MCMPC_GPU(float *h_state, SpecGPU gpu_info, curandState *devSt, 
                 dvc[blockIdx.x].u[k][i] = U_dev[k][i];
             }
         }
-	printf("ID: %d Value: %f Mean: %f\n", id, U_dev[NO1][0], InpSeq[0].u[0]);
+	printf("ID: %d Value: %f Mean: %f\n", id, U_dev[0][0], InpSeq[0].u[0]);
         //dvc[blockIdx.x] = in_block;
     }
 } 
